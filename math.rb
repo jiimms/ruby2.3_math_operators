471c1b37f1d8684c11c704116b0667935cecd72a
jiimms <jiimms@yahoo.com>
2016-02-29 15:43:40 +0530

    ruby2.3

---
+++ math.rb
@@ -0,0 +1,11 @@
+
+	def calc(a,b)
+		puts "addition"+" " + (a+b).to_s
+		puts "subtraction"+" " + (a-b).to_s
+		puts "multiplication"+" " + (a*b).to_s
+		puts "division"+" " + (a/b).to_s
+	end
+	calc(20,10)
+	
+	
+		
